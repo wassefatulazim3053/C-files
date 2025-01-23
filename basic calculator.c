98e31c75cb1bcc581be513a4a48a283408b9f491
wassefatulazim3053 <wassefatulazim3053@gmail.com>
2025-01-23 22:10:05 +0600

    Add files via upload

---
+++ basic calculator.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if(num2 != 0) {
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            } else {
+                printf("0\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
